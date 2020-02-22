8993c0376429ce3ac10223b384def28371da59ec
Lacour Vincent <lacour.vincent.app@gmail.com>
2020-02-22 17:56:50 +0100

    Add user and password regex

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 ## Basics
 
-|               Code                | Description                  |
-| :-------------------------------: | ---------------------------- |
-|                `.`                | Any character except newline |
-|                `a`                | The character a              |
-|               `ab`                | The string ab                |
-|               `a|b`               | a or b                       |
-|               `a*`                | 0 or more a                  |
-| `\` | Escapes a special character |
+|  Code  | Description                  |
+| :----: | ---------------------------- |
+|  `.`   | Any character except newline |
+|  `a`   | The character a              |
+|  `ab`  | The string ab                |
+| `a\|b` | a or b                       |
+|  `a*`  | 0 or more a                  |
+|  `\`   | Escapes a special character  |
 
 ## Quantifiers
 
@@ -18,9 +18,9 @@
 |   `*`   | 0 or more       |
 |   `+`   | 1 or more       |
 |   `?`   | 0 or 1          |
-|  `{1]`  | Exactly 1       |
-| `{1,3]` | Between 1 and 3 |
-| `{1,]`  | 1 or more       |
+|  `{1}`  | Exactly 1       |
+| `{1,3}` | Between 1 and 3 |
+| `{1,}`  | 1 or more       |
 
 ## Groups and Ranges
 
@@ -37,14 +37,14 @@
 
 ## Classes
 
-| Code | Description            |
-| :--: | ---------------------- |
-| `\d` | One digit              |
-| `\D` | One non-digit          |
-| `\s` | One whitespace         |
-| `\S` | One non-whitespace     |
-| `\w` | One word character     |
-| `\W` | One non-word character |
+| Code  | Description            |
+| :---: | ---------------------- |
+| `\d`  | One digit              |
+| `\D`  | One non-digit          |
+| `\s`  | One whitespace         |
+| `\S`  | One non-whitespace     |
+| `\w`  | One word character     |
+| `\W`  | One non-word character |
 
 ## Assertions
 
@@ -58,3 +58,13 @@
 | `(?<=...)` | Positive lookbehind |
 | `(?!...)`  | Negative lookahead  |
 | `(?<!...)` | Negative lookbehind |
+
+## Examples
+
+|   Name    | Expression                                                |
+| :-------: | --------------------------------------------------------- |
+| Username  | `/^([a-zA-Z0-9_-]){2,15}$/`                               |
+| Password  | `/^(?=.*[a-z]{1,})(?=.*[A-Z]{1,})(?=.*[0-9]{1,})\w{8,}$/` |
+| Birthdate | `/^(0[1-9]\|[12][0-9]\|3[01])\/(0[1-9]\|1[0-2])\/\d{4}$/` |
+|  Zipcode  | `/^\d{5}$/`                                               |
+
--- test/regex.test.ts
+++ test/regex.test.ts
@@ -1,24 +1,98 @@
+describe("Username", () => {
+  const USERNAME_REGEX = /^([a-zA-Z0-9_-]){2,15}$/;
+
+  it("should return true - classic username", () => {
+    const username = "my-user-name";
+    const isUsername = USERNAME_REGEX.test(username);
+    expect(isUsername).toBe(true);
+  });
+
+  it("should return false - too short username", () => {
+    const username = "Z";
+    const isUsername = USERNAME_REGEX.test(username);
+    expect(isUsername).toBe(false);
+  });
+
+  it("should return false - too long username", () => {
+    const username = "user-name-very-long-with-a-lot-of-characters";
+    const isUsername = USERNAME_REGEX.test(username);
+    expect(isUsername).toBe(false);
+  });
+
+  it("should return false - special characters", () => {
+    const username = "$my-username";
+    const isUsername = USERNAME_REGEX.test(username);
+    expect(isUsername).toBe(false);
+  });
+});
+
+describe("Password", () => {
+  const PASSWORD_REGEX = /^(?=.*[a-z]{1,})(?=.*[A-Z]{1,})(?=.*[0-9]{1,})\w{8,}$/;
+
+  it("should return true - strong password", () => {
+    const password = "myPassword42";
+    const isPassword = PASSWORD_REGEX.test(password);
+    expect(isPassword).toBe(true);
+  });
+
+  it("should return false - missing at least one uppercase", () => {
+    const password = "mypassword42";
+    const isPassword = PASSWORD_REGEX.test(password);
+    expect(isPassword).toBe(false);
+  });
+
+  it("should return false - missing at least one digit", () => {
+    const password = "mypassword";
+    const isPassword = PASSWORD_REGEX.test(password);
+    expect(isPassword).toBe(false);
+  });
+
+  it("should return false - missing at least one lowercase", () => {
+    const password = "MYPASSWORD42";
+    const isPassword = PASSWORD_REGEX.test(password);
+    expect(isPassword).toBe(false);
+  });
+});
+
 describe("Birthdate", () => {
-  const BIRTHDATE_REGEX = /([0-1][1-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}/;
-  it("should return true", () => {
+  const BIRTHDATE_REGEX = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
+  it("should return true - day inferior to 10", () => {
     const birthdate = "01/01/1990";
     const isBirthdate = BIRTHDATE_REGEX.test(birthdate);
     expect(isBirthdate).toBe(true);
   });
 
-  it("should return false with wrong day", () => {
+  it("should return true - day between 10 and 20", () => {
+    const birthdate = "10/01/1990";
+    const isBirthdate = BIRTHDATE_REGEX.test(birthdate);
+    expect(isBirthdate).toBe(true);
+  });
+
+  it("should return true - day betwwen 20 and 30", () => {
+    const birthdate = "22/01/1990";
+    const isBirthdate = BIRTHDATE_REGEX.test(birthdate);
+    expect(isBirthdate).toBe(true);
+  });
+
+  it("should return true - day superior to 30", () => {
+    const birthdate = "31/01/1990";
+    const isBirthdate = BIRTHDATE_REGEX.test(birthdate);
+    expect(isBirthdate).toBe(true);
+  });
+
+  it("should return false - day superior to 31", () => {
     const birthdate = "32/01/1990";
     const isBirthdate = BIRTHDATE_REGEX.test(birthdate);
     expect(isBirthdate).toBe(false);
   });
 
-  it("should return false with wrong month", () => {
+  it("should return false - month superior to 12", () => {
     const birthdate = "01/13/1990";
     const isBirthdate = BIRTHDATE_REGEX.test(birthdate);
     expect(isBirthdate).toBe(false);
   });
 
-  it("should return false with wrong year", () => {
+  it("should return false - year with 3 digits", () => {
     const birthdate = "01/01/990";
     const isBirthdate = BIRTHDATE_REGEX.test(birthdate);
     expect(isBirthdate).toBe(false);
@@ -26,15 +100,21 @@ describe("Birthdate", () => {
 });
 
 describe("Zipcode", () => {
-  const ZIPCODE_REGEX = /\d{5}/;
-  it("should return true", () => {
+  const ZIPCODE_REGEX = /^\d{5}$/;
+  it("should return true - classic zipcode", () => {
     const zipcode = "75000";
     const isZipcode = ZIPCODE_REGEX.test(zipcode);
     expect(isZipcode).toBe(true);
   });
 
-  it("should return false", () => {
+  it("should return false - no digits found", () => {
     const zipcode = "acbcd";
+    const isZipcode = ZIPCODE_REGEX.test(zipcode);
+    expect(isZipcode).toBe(false);
+  });
+
+  it("should return false - zipcode with 4 digits", () => {
+    const zipcode = "7500";
     const isZipcode = ZIPCODE_REGEX.test(zipcode);
     expect(isZipcode).toBe(false);
   });
